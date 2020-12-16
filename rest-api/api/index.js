fd0df1eeefac107625c167ff019961e5fb289bd3
Salman Ahmed <salmanahmed4858@gmail.com>
2020-12-16 22:01:08 +0500

    Add rest API section using expressJS.

---
+++ rest-api/api/index.js
@@ -0,0 +1,17 @@
+const port = process.env.PORT || 9000;
+const express = require('express');
+
+const app = express();
+
+// home route
+app.get('/', (req, res) => {
+    res.send('Welcome to rest api.')
+});
+
+// route to get users is
+app.get('/user/ids', (req, res) => {
+    res.send([1,2,3,4,5]);
+});
+
+
+app.listen(port, () => { console.log('Server Start listening Properly..')});
---
+++ rest-api/rest-api.md
@@ -0,0 +1,9 @@
+In this section we learn:
+
+How to use express framework?
+How to create rest apis?
+What is Express?
+
+Express is a fast and light weight web framework. 
+It is a most popular web framework. 
+It gives a project a proper structure.
