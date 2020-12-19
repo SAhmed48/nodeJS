997ca96cf666503fcc7f255ed2c84a90aaa19ed8
Salman Ahmed <salmanahmed4858@gmail.com>
2020-12-19 15:53:40 +0500

    Add multiple promises example

--- async-javascript/promise.js
+++ async-javascript/promise.js
@@ -1,4 +1,4 @@
-const prmse = new Promise((resolve, reject) => {
+const promise = new Promise((resolve, reject) => {
 
     // some async code
     setTimeout(() => {
@@ -7,5 +7,35 @@ const prmse = new Promise((resolve, reject) => {
     // reject(new Error('Something went wrong.'));
 });
 
-prmse.then(resp => { console.log("promise Success.")})
-     .catch(err => { console.log("promise Error")})
+promise.then(resp => { console.log("promise Success.")})
+       .catch(err => { console.log("promise Error")})
+
+
+// Multiple backend calls handle using promises
+
+function getUser(id){
+    return new Promise((resolve, reject) => {
+        resolve({id: id, username: 'dev-salmanahmed'});
+    });
+}
+
+function getUserProfileDetails(id){
+    return new Promise((resolve, reject) => {
+        resolve({id: id, username: 'dev-salmanahmed', address: 'xyz' });
+    });
+}
+
+function getUserFullAddress(id){
+    return new Promise((resolve, reject) => {
+        resolve({id: id, city: 'xyz', state:'abc', country: 'cdfe'});
+    });
+}
+
+const user = getUser(id);
+const profileDetails = getUserProfileDetails(user.id);
+const fullAddress = getUserFullAddress(user.id);
+
+user.then(response => getUserProfileDetails(user.id))
+    .then(resp => getUserFullAddress(resp.id))
+    .then(res => console.log(res))
+    .catch(err => console.log('Error occurs', err));
